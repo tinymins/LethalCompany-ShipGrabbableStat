6e80546e1aa57592e811f551cdf6d49c8be71693
Emil Zhai <root@derzh.com>
2024-01-23 17:33:11 +0800

    feat: add full vanilla items as default

--- ShipGrabbableStat/ShipGrabbableStat.cs
+++ ShipGrabbableStat/ShipGrabbableStat.cs
@@ -20,7 +20,7 @@ namespace ShipGrabbableStat
         private void Awake()
         {
             Log = Logger;
-            StatGrabbables = Config.Bind("Settings", "StatGrabbables", "Pro-flashlight,Shovel,Stun grenade::Stun Grenade,Spray paint::Spray Paint,TZP-Inhalant,Rocket Launcher,Jetpack,Boombox,Lockpicker,Extension ladder::Extension Ladder", "Grabbable names which will be stated while scan in ship, splitted by \",\", use \"::\" to set name alias for display.");
+            StatGrabbables = Config.Bind("Settings", "StatGrabbables", "Walkie-talkie,Flashlight,Shovel,Lockpicker,Pro-flashlight,Stun grenade::Stun Grenade,Boombox,TZP-Inhalant,Zap gun,Jetpack,Extension ladder::Extension Ladder,Radar-booster,Spray paint::Spray Paint,Rocket Launcher,Flaregun,Emergency Flare (ammo),Toy Hammer,Remote Radar,Utility Belt,Hacking Tool,Pinger,Portable Tele,Advanced Portable Tele,Night Vision Goggles,Medkit,Peeper,Helmet,Diving Kit,Wheelbarrow,Ouija Board,Shells", "Grabbable names which will be stated while scan in ship, splitted by \",\", use \"::\" to set name alias for display.");
             HideZeroItems = Config.Bind("Settings", "HideZeroItems", true, "Hide grabbable item stat if its count is zero.");
 
             Harmony harmony = new Harmony(GUID);
